8a3aa19145257488aace52f0f4c7810699e42df7
Miles Weber <vorvon4@gmail.com>
2023-11-17 14:28:16 -0500

    Create Patient model and controller

---
+++ app/Models/Patient.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    /**
+     *  first_name,
+     *  last name,
+     *  email,
+     *  patient id (PK) , 
+     * phone number, 
+     * password, 
+     * date of birth, 
+     * family code, 
+     * emergency contact, 
+     * relation to emergency contact, 
+     * role, 
+     * Group, 
+     * Admission Date
+     */
+    use HasFactory;
+    protected $fillable = [
+        'patient_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'password',
+        'dob',
+        'family_code',
+        'emergency_contact',
+        'emergency_contact_relationship',
+        'role',
+        'group',
+        'admission_date'
+    ];
+
+}
