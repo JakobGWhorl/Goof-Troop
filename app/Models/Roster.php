510fc115f6532444279b4995b04c52831e6d54cf
JakobGWhorl <jakesterw1@hotmail.com>
2023-11-20 11:53:21 -0500

    jakes models and controllers

---
+++ app/Models/Roster.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Roster extends Model
+{
+    use HasFactory;
+    protected $fillable = ['roster_id','date','supervisor','doctor','caregiver1','caregiver2','caregiver3','caregiver4'];
+}
