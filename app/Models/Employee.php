b6a331a83ba0e05f44f9cc11ec8f8dac2a1d70ca
Miles Weber <vorvon4@gmail.com>
2023-11-20 13:46:04 -0500

    Started middleware nonsense

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Notifications\Notifiable;
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+}
 
 class Employee extends Model
 {
