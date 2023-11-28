812ece749cbd1d4e05380c57334e0424c132b9f1
JakobGWhorl <jakesterw1@hotmail.com>
2023-11-28 13:24:01 -0500

    working changes homepage

---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href={{ URL::asset('css/'); }}>
+    <title>Home Page</title>
+</head>
+<body>
+<h1>Welcome</h1>
+    <a href=""> <button>Login</button> </a>
+    <br>
+    <br>
+    <a href=""> <button>Register</button> </a>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\Roster_Controller;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HomePage');
 });
 
 
